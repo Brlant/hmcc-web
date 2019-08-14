a54244bfda4c7812fe8e658d852efe1d922fc590
zaoyu63 <zaoyu63@163.com>
2019-08-14 13:47:14 +0800

    设备监控, 不在判断探头告警

--- src/components/monitoring/equipment/dev-list.vue
+++ src/components/monitoring/equipment/dev-list.vue
@@ -2,20 +2,25 @@
   .dev-list {
     margin-top: 10px;
   }
+
   .dev-list {
     display: flex;
     justify-content: space-between;
+
     .item {
       padding: 0 10px;
+
       .content {
         background: #f2f6fc;
         border-radius: 4px;
         padding: 5px;
       }
     }
+
     .fl {
       float: left;
     }
+
     .fr {
       float: right;
     }
@@ -25,7 +30,7 @@
   <el-row class="dev-list">
     <el-col class="item" :span="12" :key="item.id" v-for="item in devItem.sensorDataList">
       <div class="content">
-        <el-row >
+        <el-row>
           <el-col :span="12">
             <oms-row label="设备" :span="8">
               <el-tooltip effect="dark" placement="top" :content="item.name + ' ' + item.no + ' ' + item.type ">
@@ -43,8 +48,9 @@
           <el-col :span="8">
             <oms-row label="温度" :span="12">
               <template v-if="item.temperature !== null">
-                <span class="fl" :class="{'text-danger': item.alarm}">{{item.temperature}} ℃</span>
-                <el-tooltip effect="dark" placement="top" :content="`温度范围：${item.temperatureMin}~${item.temperatureMax}℃`">
+                <span class="fl">{{item.temperature}} ℃</span>
+                <el-tooltip effect="dark" placement="top"
+                            :content="`温度范围：${item.temperatureMin}~${item.temperatureMax}℃`">
                   <i class="el-icon-warning-outline ml-10"/>
                 </el-tooltip>
               </template>
@@ -56,7 +62,7 @@
           <el-col :span="8">
             <oms-row label="湿度" :span="12">
               <template v-if="item.humidity !== null">
-                <span class="fl" :class="{'text-danger': item.alarm}">{{item.humidity}} %</span>
+                <span class="fl">{{item.humidity}} %</span>
                 <el-tooltip effect="dark" placement="top" :content="`湿度范围：${item.humidityMin}~${item.humidityMax}%`">
                   <i class="el-icon-warning-outline ml-10"/>
                 </el-tooltip>
@@ -65,7 +71,7 @@
             </oms-row>
           </el-col>
           <el-col :span="8">
-            <oms-row label="电压" :span="12" :class="{'text-danger': item.alarm}">
+            <oms-row label="电压" :span="12">
               <template v-if="item.voltage !== null">
                 <span class="fl">{{item.voltage}} V</span>
                 <el-tooltip effect="dark" placement="top" :content="`电压范围：${item.voltageMin}~${item.voltageMax}V`">
@@ -77,7 +83,7 @@
           </el-col>
         </el-row>
         <el-row v-show="item.lastUpdateTime">
-          <el-col :span="12" >
+          <el-col :span="12">
             <oms-row label="更新" :span="8">{{item.lastUpdateTime | time}}</oms-row>
           </el-col>
         </el-row>
@@ -89,6 +95,6 @@
   export default {
     props: {
       devItem: Object
-    },
+    }
   };
 </script>
