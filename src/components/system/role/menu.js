31cb31b1fd2a12ebff982728229de9badc0cd698
zaoyu63 <zaoyu63@163.com>
2019-08-14 12:05:40 +0800

    微信权限

--- src/components/system/role/menu.js
+++ src/components/system/role/menu.js
@@ -294,7 +294,21 @@ export default [
             'id': 'ccs-wachart-qr-code',
             'label': '绑定微信',
             'parentId': 'hmcc-system'
-          },
+          }, {
+            'id': 'ccs-wachat-qr-code',
+            'parentId': 'ccs-notify-manager',
+            'label': '获取微信码',
+            'sort': null,
+            'children': null,
+            'leaf': true
+          }, {
+            'id': 'ccs-wachart-ticket',
+            'parentId': 'ccs-notify-manager',
+            'label': '绑定微信',
+            'sort': null,
+            'children': null,
+            'leaf': true
+          }
         ]
       },
       {
